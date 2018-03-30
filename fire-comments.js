9874beb1f05c84cccb7475587168649f708fe1db
Akshay Dhiman <34573958+theakshaydhiman@users.noreply.github.com>
2018-03-30 19:22:16 +0530

    v0.2.0

--- fire-comments.js
+++ fire-comments.js
@@ -1,169 +1,178 @@
-/*
-* Fire Comments JS v0.1.0 (https://github.com/theakshaydhiman/Fire-Comments-JS)
+/* 
+* Fire Comments JS v0.2.0 (https://github.com/theakshaydhiman/Fire-Comments-JS)
 * Copyright 2018 Akshay Dhiman
 * MIT License (https://github.com/theakshaydhiman/Fire-Comments-JS/blob/master/LICENSE)
 */
 
-// Create a Firebase Realtime Database compatible version of the URL
-let slugify = text => text.toString().toLowerCase().trim().replace(/&/g, '-and-').replace(/[\s\W-]+/g, '-').replace(/[^a-zA-Z0-9-_]+/g,'');
+( () => {
 
-// Get Element ID
-let id = id => document.getElementById(id);
+  // Create a Firebase Realtime Database compatible version of the URL
+  const slugify = text => text
+    .toString()
+    .toLowerCase()
+    .trim()
+    .replace(/&/g, '-and-')
+    .replace(/[\s\W-]+/g, '-')
+    .replace(/[^a-zA-Z0-9-_]+/g, '');
 
-// Get Value of an Element
-let getVal = id => document.getElementById(id).value;
+  // Get Element ID
+  const id = gId => document.getElementById(gId);
 
-// References
-const commentsRef = firebase.database().ref('comments').child(slugify(window.location.pathname));
-const commentsReplyRef = firebase.database().ref('replies').child(slugify(window.location.pathname));
+  // Get Value of an Element
+  const getVal = id => document.getElementById(id).value;
 
-// Fuction to submit a new comment
-let saveComment = (name, md5Email, message, where) => {
-  where.push().set({
-    name:name,
-    message:message,
-    md5Email:md5Email,
-    postedAt: firebase.database.ServerValue.TIMESTAMP
+  // References
+  const commentsRef = firebase.database().ref('comments').child(slugify(window.location.pathname));
+
+  // Fuction to submit a new comment
+  const saveComment = (name, md5Email, message, where) => {
+    where.push().set({
+      name: name,
+      message: message,
+      md5Email: md5Email,
+      postedAt: firebase.database.ServerValue.TIMESTAMP,
+    });
+  };
+
+  // Submit new comment at the bottom
+  id('comment').addEventListener('submit', (e) => {
+    let name = getVal('name');
+    let email = getVal('email');
+    let message = getVal('message');
+    let md5Email = md5(email);
+
+    e.preventDefault();
+    saveComment(name, md5Email, message, commentsRef);
+    alert('Your comment has been submitted!');
+    id('comment').reset();
   });
-}
 
-// Submit new comment at the bottom
-id('comment').addEventListener('submit', (e) => {
-  let name = getVal('name');
-  let email = getVal('email');
-  let message = getVal('message');
-  let md5Email = md5(email);
 
-  e.preventDefault();
-  saveComment(name, md5Email, message, commentsRef);
-  alert('Your comment has been submitted!');
-  id('comment').reset();
-});
+  // 'linkKey' - stores the clicked reply position
+  // 'fexecuted' - boolean, limits the function saveReply to be executed only once per submit event
 
-/*
-* 'linkKey' - stores the clicked reply position
-* 'fexecuted' - boolean, limits the function saveReply to be executed only once per submit event
-*/
-let linkKey, fexecuted;
+  let linkKey;
+  let fexecuted;
 
-// Fuction to submit a new reply
-let saveReply = (name, md5Email, message) => {
-  if (!fexecuted) {
-    commentsRef.child(linkKey).child('replies').push().set({
-      name:name,
-      message:message,
-      md5Email:md5Email,
-      postedAt: firebase.database.ServerValue.TIMESTAMP
-    });
-    alert('Your reply has been submitted!');
-    window.fexecuted = true;
-  }
-}
+  // Fuction to submit a new reply
+  let saveReply = (name, md5Email, message) => {
+    if (!fexecuted) {
+      commentsRef.child(linkKey).child('replies').push().set({
+        name:name,
+        message:message,
+        md5Email:md5Email,
+        postedAt: firebase.database.ServerValue.TIMESTAMP
+      });
+      alert('Your reply has been submitted!');
+      window.fexecuted = true;
+    }
+  };
 
-// Reply function
-document.addEventListener('click', e => {
+  // Reply function
+  document.addEventListener('click', e => {
 
-  let targetParent = null;
-  
-  // Grabbing the clicked "Reply" button
-  if(e.target.id === "creply"){
-
-    // Find the key of the clicked "Reply" button
-    linkKey = e.target.querySelector('#ckey').innerText;
-    console.log(linkKey);
-    window.fexecuted = false;
-
-    // Get the reply form
-    id('reply-form-title').innerText = 'Reply';
-    e.target.insertAdjacentElement('afterend', id('reply'));
-    id('reply').style.display = 'block';
-    id('comment').style.display = 'none';
+    let targetParent = null;
     
-    // Submit reply
-    id('reply').addEventListener('submit', e => {
-      let name = getVal('name');
-      let email = getVal('email');
-      let message = getVal('message');
-      let md5Email = md5(email);
+    // Grabbing the clicked "Reply" button
+    if(e.target.id === "creply"){
 
-      e.preventDefault();
-      saveReply(name, md5Email, message);
-      id('reply').reset();
+      // Find the key of the clicked "Reply" button
+      linkKey = e.target.querySelector('#ckey').innerText;
+      console.log(linkKey);
+      window.fexecuted = false;
 
-      id('reply').style.display = 'none';
-      id('comment').style.display = 'block';
-    });
+      // Get the reply form
+      id('reply-form-title').innerText = 'Reply';
+      e.target.insertAdjacentElement('afterend', id('reply'));
+      id('reply').style.display = 'block';
+      id('comment').style.display = 'none';
+      
+      // Submit reply
+      id('reply').addEventListener('submit', e => {
+        let name = getVal('name');
+        let email = getVal('email');
+        let message = getVal('message');
+        let md5Email = md5(email);
 
-    // Cancel Reply
-    id('cancel-reply').addEventListener('click', () => {
-      id('reply').style.display = 'none';
-      id('comment').style.display = 'block';
-      window.linkKey = null;
-    });
+        e.preventDefault();
+        saveReply(name, md5Email, message);
+        id('reply').reset();
+
+        id('reply').style.display = 'none';
+        id('comment').style.display = 'block';
+      });
+
+      // Cancel Reply
+      id('cancel-reply').addEventListener('click', () => {
+        id('reply').style.display = 'none';
+        id('comment').style.display = 'block';
+        window.linkKey = null;
+      });
+      
+    }
+  });
+
+  // Variable initialization to count the number of replies only
+  let repCount = 0;
+
+  // Display comments
+  let ulList = id('comments-list');
+  commentsRef.on('child_added', snap => {
     
-  }
-});
+    let key = snap.key;
+    let c = snap.val();
+    let li = document.createElement('li');
 
-// Variable initialization to count the number of replies only
-let repCount = 0;
-
-// Display comments
-let ulList = id('comments-list');
-commentsRef.on('child_added', snap => {
-  
-  let key = snap.key;
-  let c = snap.val();
-  let li = document.createElement('li');
-
-  // Make links nofollow
-  c.message = c.message.replace('<a ', '<a target="_blank" rel="nofollow noopener" ');
-
-  let html = "<div class='comment-item'>";
-  html += "<div class='left'><img class='author-grav' src='https://www.gravatar.com/avatar/" + c.md5Email + "?s=80&d=retro'/></div>";
-  html += "<h3>" + xssFilters.inHTMLData(c.name) + "</h3>";
-  html += "<small>" + timeago().format(c.postedAt) + "</small>";
-  html += "<p>" + xssFilters.inHTMLComment(c.message) + "</p>";
-  html += "<button id='creply' class='comment-reply btn'><span id='ckey' style='display:none;'>" + key + "</span>Reply</button>";
-  html += "</div>";
-
-  // Append values to a new list item
-  li.innerHTML = html;
-  ulList.appendChild(li);
-
-  // Function to count the number of replies only
-  let countReplies = v => repCount = repCount + v;
-
-  // Display replies
-  let thisReplyRef = firebase.database().ref('comments/' + slugify(window.location.pathname) + '/' + key + '/replies');
-  thisReplyRef.on('child_added', snap => {
-    
-    let r = snap.val();
-    let liRep = document.createElement('li');
-  
     // Make links nofollow
-    r.message = r.message.replace('<a ', '<a target="_blank" rel="nofollow noopener" ');
-  
-    let html = "<div class='reply-item'>";
-    html += "<div class='left'><img class='author-grav' src='https://www.gravatar.com/avatar/" + r.md5Email + "?s=80&d=retro'/></div>";
-    html += "<h3>" + xssFilters.inHTMLData(r.name) + "</h3>";
-    html += "<small>" + timeago().format(r.postedAt) + "</small>";
-    html += "<p>" + xssFilters.inHTMLComment(r.message) + "</p>";
-    html += "<button id='creply' class='comment-reply btn'><span id='ckey' style='display:none;'>" + key + "</span>Reply</button>";
-    html += "</div>";
-  
+    c.message = c.message.replace('<a ', '<a target="_blank" rel="nofollow noopener" ');
+
+    let html = `<div class="comment-item">
+    <div class="left"><img class="author-grav" src="https://www.gravatar.com/avatar/${c.md5Email}?s=80&d=retro/"></div>
+    <h3>${xssFilters.inHTMLData(c.name)}</h3>
+    <small>${timeago().format(c.postedAt)}</small>
+    <p>${xssFilters.inHTMLComment(c.message)}</p>
+    <button id="creply" class="comment-reply btn"><span id="ckey" style="display:none;">${key}</span>Reply</button>
+    </div>`;
+
     // Append values to a new list item
-    liRep.innerHTML = html;
-    ulList.appendChild(liRep);
+    li.innerHTML = html;
+    ulList.appendChild(li);
 
-    countReplies(1);
+    // Function to count the number of replies only
+    let countReplies = v => repCount = repCount + v;
+
+    // Display replies
+    let thisReplyRef = firebase.database().ref('comments/' + slugify(window.location.pathname) + '/' + key + '/replies');
+    thisReplyRef.on('child_added', snap => {
+      
+      let r = snap.val();
+      let liRep = document.createElement('li');
+    
+      // Make links nofollow
+      r.message = r.message.replace('<a ', '<a target="_blank" rel="nofollow noopener" ');
+    
+      let html = `<div class="reply-item">
+      <div class="left"><img class="author-grav" src="https://www.gravatar.com/avatar/${r.md5Email}?s=80&d=retro"></div>
+      <h3>${xssFilters.inHTMLData(r.name)}</h3>
+      <small>${timeago().format(r.postedAt)}</small>
+      <p>${xssFilters.inHTMLComment(r.message)}</p>
+      <button id="creply" class="comment-reply btn"><span id="ckey" style="display:none;">${key}</span>Reply</button>
+      </div>`;
+    
+      // Append values to a new list item
+      liRep.innerHTML = html;
+      ulList.appendChild(liRep);
+
+      countReplies(1);
+    });
   });
-});
 
-// Count the number of comments
-commentsRef.once("value").then( snap => {
-    id('comments-count').innerText = snap.numChildren() + repCount;
-});
+  // Count the number of comments
+  commentsRef.once("value").then( snap => {
+      id('comments-count').innerText = snap.numChildren() + repCount;
+  });
 
+})();
 
 // Bugs:
 // 1. Reply when submitted doesn't show in right order unless the page is refreshed.
