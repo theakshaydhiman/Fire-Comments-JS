dc671629101620b6991556edb14a4f7779ad5065
Akshay Dhiman <34573958+theakshaydhiman@users.noreply.github.com>
2018-04-08 20:20:17 +0530

    0.3.0

--- fire-comments.js
+++ fire-comments.js
@@ -1,202 +1,225 @@
 /* 
-* Fire Comments JS v0.2.3 (https://github.com/theakshaydhiman/Fire-Comments-JS)
+* Fire Comments JS v0.3.0 (https://github.com/theakshaydhiman/Fire-Comments-JS)
 * Copyright 2018 Akshay Dhiman
 * MIT License (https://github.com/theakshaydhiman/Fire-Comments-JS/blob/master/LICENSE)
 */
-
-
 
 ( () => {
 
   "use strict";
 
-  // Create a Firebase Realtime Database compatible version of the URL.
-  const slugify = (text) => text
-    .toString()
-    .toLowerCase()
-    .trim()
-    .replace(/&/g, '-and-')
-    .replace(/[\s\W-]+/g, '-')
-    .replace(/[^a-zA-Z0-9-_]+/g, '');
+  class Globals {
+    constructor() {
 
-  // Get Element ID.
-  const id = gId => document.getElementById(gId);
+      // DOM variables.
+      this.comment = this.id('comment');
+      this.reply = this.id('reply');
+      this.commentsCount = this.id('comments-count');
+      this.ulList = this.id('comments-list');
+      this.cancelReply = this.id('cancel-reply');
 
-  // Get Value of an Element.
-  const getVal = id => document.getElementById(id).value;
+      // Reference to the current URL.
+      this.commentsRef = firebase.database().ref('comments').child(this.slugify(window.location.pathname));
+    }
 
-  // DOM Variables.
-  const comment = id('comment');
-  const reply = id('reply');
-  const replyFormTitle = id('reply-form-title');
-  const commentsCount = id('comments-count');
+    // Creates a Firebase Realtime Database compatible version of the URL.
+    slugify(text) {
+      return text
+        .toString()
+        .toLowerCase()
+        .trim()
+        .replace(/&/g, '-and-')
+        .replace(/[\s\W-]+/g, '-')
+        .replace(/[^a-zA-Z0-9-_]+/g, '');
+    }
+
+    id(gId) {
+      return document.getElementById(gId);
+    }
+
+    getVal(id) {
+      return document.getElementById(id).value;
+    }
+
+    // Display the total number of comments to #comments-count.
+    commentsCountShow(c) {
+      this.commentsCount.innerText = c;
+    }
+  }
+
+  class Submit extends Globals {
+    constructor() {
+      super();
+
+      // Boolean. Limits the function saveReply to be executed only once per submit event.
+      this.fexecuted = true;
+
+      // Stores the clicked reply key in the database.
+      this.linkKey = '';
+    }
+
+    // Fuction to submit a new comment.
+    saveComment(name, md5Email, message) {
+      this.commentsRef.push().set({
+        name: name,
+        message: message,
+        md5Email: md5Email,
+        postedAt: firebase.database.ServerValue.TIMESTAMP
+      });
+    }
+
+    // Fuction to submit a new reply.
+    saveReply(name, md5Email, message) {
+      if (!this.fexecuted) {
+        this.commentsRef.child(this.linkKey).child('replies').push().set({
+          name: name,
+          message: message,
+          md5Email: md5Email,
+          postedAt: firebase.database.ServerValue.TIMESTAMP
+        });
+      }
+    }
+
+    showReplyForm() {
+      this.reply.style.display = 'block';
+      this.comment.style.display = 'none';
+    }
+
+    hideReplyForm() {
+      this.reply.style.display = 'none';
+      this.comment.style.display = 'block';
+    }
+  }
+
+  class Display extends Globals {
+    constructor(snap) {
+      super();
+      this.key = snap.key;
+      this.c = snap.val();
+      this.li = document.createElement('li');
+      this.html = '';
+    }
+
+    makeNoFollow(message) {
+      message = message.replace('<a ', '<a target="_blank" rel="nofollow noopener" ');
+      return message;
+    }
+
+    showComments() {
+      // Make links nofollow.
+      this.c.message = this.makeNoFollow(this.c.message);
+      
+      // Create comment item markup.
+      this.html = `<div class="comment-item">
+      <div class="left"><img class="author-grav" src="https://www.gravatar.com/avatar/${this.c.md5Email}?s=80&d=retro"></div>
+      <h3>${xssFilters.inHTMLData(this.c.name)}</h3>
+      <small>${timeago().format(this.c.postedAt)}</small>
+      <p>${xssFilters.inHTMLComment(this.c.message)}</p>
+      <button id="creply" class="comment-reply btn"><span id="ckey" style="display:none;">${this.key}</span>Reply</button>
+      </div>`;
   
-  // Reference to current URL.
-  const commentsRef = firebase.database().ref('comments').child(slugify(window.location.pathname));
+      // Append values to a new list item.
+      this.li.innerHTML = this.html;
+      this.ulList.appendChild(this.li);
 
-  // Fuction to submit a new comment.
-  const saveComment = (name, md5Email, message, where) => {
-    where.push().set({
-      name: name,
-      message: message,
-      md5Email: md5Email,
-      postedAt: firebase.database.ServerValue.TIMESTAMP
-    });
-  };
+      this.showReplies();
+    }
 
-  // Submit new comment at the bottom.
+    showReplies() {
+      let thisReplyRef = firebase.database().ref('comments/' + this.slugify(window.location.pathname) + '/' + this.key + '/replies');
+      thisReplyRef.on('child_added', snap => {
+        
+        let r = snap.val();
+        let liRep = document.createElement('li');
+      
+        // Make links nofollow.
+        r.message = this.makeNoFollow(r.message);
+      
+        let html = `<div class="reply-item">
+        <div class="left"><img class="author-grav" src="https://www.gravatar.com/avatar/${r.md5Email}?s=80&d=retro"></div>
+        <h3>${xssFilters.inHTMLData(r.name)}</h3>
+        <small>${timeago().format(r.postedAt)}</small>
+        <p>${xssFilters.inHTMLComment(r.message)}</p>
+        <button id="creply" class="comment-reply btn"><span id="ckey" style="display:none;">${this.key}</span>Reply</button>
+        </div>`;
+      
+        // Append values to a new list item.
+        liRep.innerHTML = html;
+        this.ulList.appendChild(liRep);
+
+        commentCount += 1;
+      });
+    }
+  }
+
+  let commentCount = 0;
+  const g = new Globals();
+  const s = new Submit();
+
+  // Listener to submit a new comment.
   comment.addEventListener('submit', (e) => {
-    let name = getVal('name');
-    let email = getVal('email');
-    let message = getVal('message');
-    let md5Email = md5(email);
+    let name = g.getVal('name');
+    let message = g.getVal('message');
+    let md5Email = md5(g.getVal('email'));
 
     e.preventDefault();
-    saveComment(name, md5Email, message, commentsRef);
+    s.saveComment(name, md5Email, message);
     alert('Your comment has been submitted!');
     comment.reset();
   });
 
-
-  // 'linkKey' - stores the clicked reply position.
-  // 'fexecuted' - boolean, limits the function saveReply to be executed only once per submit event.
-
-  let linkKey;
-  let fexecuted;
-
-  // Fuction to submit a new reply.
-  let saveReply = (name, md5Email, message) => {
-    if (!fexecuted) {
-      commentsRef.child(linkKey).child('replies').push().set({
-        name:name,
-        message:message,
-        md5Email:md5Email,
-        postedAt: firebase.database.ServerValue.TIMESTAMP
-      });
-      alert('Your reply has been submitted!');
-      window.fexecuted = true;
-    }
-  };
-
-  // Reply function.
+  // Listener to the reply button.
   document.addEventListener('click', e => {
-
-    let targetParent = null;
-    
-    // Grabbing the clicked "Reply" button.
     if(e.target.id === "creply"){
 
-      // Find the key of the clicked "Reply" button.
-      linkKey = e.target.querySelector('#ckey').innerText;
-      window.fexecuted = false;
+      // Find the key of the clicked "Reply" button and set it to linkKey.
+      s.linkKey = e.target.querySelector('#ckey').innerText;
+      s.fexecuted = false;
 
       // Get the reply form.
-      replyFormTitle.innerText = 'Reply';
-      e.target.insertAdjacentElement('afterend', id('reply'));
-      reply.style.display = 'block';
-      comment.style.display = 'none';
-      
-      // Submit reply.
-      id('reply').addEventListener('submit', e => {
-        let name = getVal('name');
-        let email = getVal('email');
-        let message = getVal('message');
-        let md5Email = md5(email);
+      e.target.insertAdjacentElement('afterend', g.reply);
+      s.showReplyForm();
 
-        e.preventDefault();
-        saveReply(name, md5Email, message);
-        reply.reset();
-
-        reply.style.display = 'none';
-        comment.style.display = 'block';
-        window.fexecuted = true;
-      });
-
-      // Cancel Reply.
-      id('cancel-reply').addEventListener('click', () => {
-        reply.style.display = 'none';
-        comment.style.display = 'block';
-        window.linkKey = null;
-        window.fexecuted = true;   
-      });
-      
     } else { return; }
   });
 
-  // Variable initialization to count the number of replies only.
-  let repCount = 0;
+  // Listener of the submit button of the reply form.
+  g.reply.addEventListener('submit', e => {
+    let name = g.getVal('name');
+    let message = g.getVal('message');
+    let md5Email = md5(g.getVal('email'));
 
-  // Display comments.
-  let ulList = id('comments-list');
-  commentsRef.on('child_added', snap => {
+    e.preventDefault();
+    s.saveReply(name, md5Email, message);
+    g.reply.reset();
+
+    s.hideReplyForm();
+
+    alert('Your reply has been submitted!');
+    s.fexecuted = true;
     
-    let key = snap.key;
-    let c = snap.val();
-    let li = document.createElement('li');
-
-    // Make links nofollow.
-    c.message = c.message.replace('<a ', '<a target="_blank" rel="nofollow noopener" ');
-
-    let html = `<div class="comment-item">
-    <div class="left"><img class="author-grav" src="https://www.gravatar.com/avatar/${c.md5Email}?s=80&d=retro"></div>
-    <h3>${xssFilters.inHTMLData(c.name)}</h3>
-    <small>${timeago().format(c.postedAt)}</small>
-    <p>${xssFilters.inHTMLComment(c.message)}</p>
-    <button id="creply" class="comment-reply btn"><span id="ckey" style="display:none;">${key}</span>Reply</button>
-    </div>`;
-
-    // Append values to a new list item.
-    li.innerHTML = html;
-    ulList.appendChild(li);
-
-    // Function to count the number of replies only.
-    let countReplies = v => repCount += v;
-
-    // Display replies.
-    let thisReplyRef = firebase.database().ref('comments/' + slugify(window.location.pathname) + '/' + key + '/replies');
-    thisReplyRef.on('child_added', snap => {
-      
-      let r = snap.val();
-      let liRep = document.createElement('li');
-    
-      // Make links nofollow.
-      r.message = r.message.replace('<a ', '<a target="_blank" rel="nofollow noopener" ');
-    
-      let html = `<div class="reply-item">
-      <div class="left"><img class="author-grav" src="https://www.gravatar.com/avatar/${r.md5Email}?s=80&d=retro"></div>
-      <h3>${xssFilters.inHTMLData(r.name)}</h3>
-      <small>${timeago().format(r.postedAt)}</small>
-      <p>${xssFilters.inHTMLComment(r.message)}</p>
-      <button id="creply" class="comment-reply btn"><span id="ckey" style="display:none;">${key}</span>Reply</button>
-      </div>`;
-    
-      // Append values to a new list item.
-      liRep.innerHTML = html;
-      ulList.appendChild(liRep);
-
-      countReplies(1);
-    });
+    // Window.reload() because of two bugs:
+    // 1. Unexpected behavior. After reply submission, if a comment is submitted, the fields are not properly submitted.
+    // 2. Bad UX. New reply is added to the bottom, even after submitting somewhere in the middle.
+    window.reload();
   });
 
-  // Count the number of comments.
-  commentsRef.once("value").then( snap => {
-      commentsCount.innerText = snap.numChildren() + repCount;
+  // Cancel reply button listener.
+  g.cancelReply.addEventListener('click', () => {
+    s.hideReplyForm();
+    s.linkKey = null;
+    s.fexecuted = true;   
+  });
+
+  // Display comments.
+  g.commentsRef.on('child_added', snap => {
+    const display = new Display(snap);
+    display.showComments();
+  });
+
+  // Show the number of comments in the markup.
+  g.commentsRef.once("value").then( snap => {
+    g.commentsCountShow(snap.numChildren() + commentCount);
   });
 
 })();
-
-/*
-Bugs:
-1. Reply when submitted doesn't show in right order unless the page is refreshed.
-2. Reply submit, then comment submit gives error.
-
-Features to be added:
-1. Receive notifications when new comments are submitted via nodemailer.
-2. Add markdown support.
-
-# v0.2.0 - v0.3.0
-* Using strict mode.
-* Fixed syntax errors.
-* Defined variables of DOM elements initially.
-
-*/
